0f2988af9fe93c0e74b2962b0e820bcce1898cd2
Łukasz-Mentel <lukasz199205@gmail.com>
2023-07-27 13:04:45 +0200

    47

--- IMS.Plugins/IMS.Plugins.InMemory/ProductTransactionRepository.cs
+++ IMS.Plugins/IMS.Plugins.InMemory/ProductTransactionRepository.cs
@@ -54,4 +54,21 @@ public class ProductTransactionRepository : IProductTransactionRepository
             DoneBy = doneBy
         });
     }
+
+    public Task SellProductAsync(string salesOrderNumber, Product product, int quantity, string doneBy)
+    {
+        this._productTransactions.Add(new ProductTransaction
+        {
+            ActivityType = ProductTransactionType.SellProduct,
+            SONumber = salesOrderNumber,
+            ProductId = product.ProductId,
+            QuantityBefore = product.Quantity,
+            QuantityAfter = product.Quantity - quantity,
+            TransactionDate = DateTime.Now,
+            DoneBy = doneBy,
+            UnitPrice = product.Price
+        });
+        
+        return Task.CompletedTask;
+    }
 }
