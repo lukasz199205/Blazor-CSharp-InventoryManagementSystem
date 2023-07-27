1b490cc6bcc3f87ead02a6de6ef78c83b5390634
Łukasz-Mentel <lukasz199205@gmail.com>
2023-07-27 12:40:32 +0200

    46

---
+++ IMS.WebApp/wwwroot/JSInterOp.js
@@ -0,0 +1,9 @@
+﻿function PreventFromSubmition(formId)
+{
+    document.getElementById(formId).addEventListener("keydown", function (event){
+        if(event.keyCode === 13){
+            event.preventDefault();
+            return false;
+        }
+    })
+}
