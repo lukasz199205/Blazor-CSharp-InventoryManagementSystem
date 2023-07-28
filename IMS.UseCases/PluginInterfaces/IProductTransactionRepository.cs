97286e447d434d421060169236518e9907dea316
Łukasz-Mentel <lukasz199205@gmail.com>
2023-07-28 12:17:24 +0200

    53

--- IMS.UseCases/PluginInterfaces/IProductTransactionRepository.cs
+++ IMS.UseCases/PluginInterfaces/IProductTransactionRepository.cs
@@ -6,4 +6,5 @@ public interface IProductTransactionRepository
 {
     Task ProduceAsync(string productionNumber, Product product, int quantity, string doneBy);
     Task SellProductAsync(string salesOrderNumber, Product product, int quantity, double unitPrice, string doneBy);
+    Task<IEnumerable<ProductTransaction>> GetProductTransactionsAsync(string productName, DateTime? dateFrom, DateTime? dateTo, ProductTransactionType? transactionType);
 }
---
+++ IMS.UseCases/Reports/Interfaces/ISearchProductTransactionsUseCase.cs
@@ -0,0 +1,8 @@
+﻿using IMS.CoreBusiness;
+
+namespace IMS.UseCases.Reports.Interfaces;
+
+public interface ISearchProductTransactionsUseCase
+{
+    Task<IEnumerable<ProductTransaction>> ExecuteAsync(string productName, DateTime? dateFrom, DateTime? dateTo, ProductTransactionType? transactionType);
+}
